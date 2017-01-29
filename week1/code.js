17d581302ae9b2adf2bdf3b166100af03069f503
Vitaliy <vpetruk.osn@gmail.com>
2017-01-29 19:18:43 +0200

    did about us section

--- week1/code.js
+++ week1/code.js
@@ -21,3 +21,25 @@ function myMap() {
     }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+//slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("progress");
+
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+     y[i].src ="images/p_1.png"  
+  }
+  x[slideIndex-1].style.display = "inline-block"; 
+  y[slideIndex-1].src = "images/p_0.png";  
+}
